1b027c9bf2ce3b2be081a75a99778ddab9b16047
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-03-26 11:36:32 +0200

    first commit

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <nav>
+        <x-nav-link>
+            <x-slot:link>/</x-slot>
+            Home
+        </x-nav-link>
+         <x-nav-link>
+            <x-slot:link>/about</x-slot>
+            About
+        </x-nav-link>
+         <x-nav-link>
+            <x-slot:link>/contact</x-slot>
+            Contact
+        </x-nav-link>
+    </nav>
+
+    {{ $slot }}
+</body>
+
+</html>
