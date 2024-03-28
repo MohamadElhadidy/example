6ccc2cf8b6fc29b033d076e3df140ebdc720a3db
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-03-28 10:58:17 +0200

    day six complete

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,9 +1,4 @@
-@props(['active' => false, 'type' => 'link'])
+@props(['active' => false])
 
-@if ($type == 'button')
-  <button class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</button>
-@else
-    <a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-        aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
-@endif
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
---
+++ resources/views/job.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:heading>Job Listing</x-slot:heading>
+    <h2 class="font-bold text-lg">{{$job['title']}}</h2>
+    <p> This job Pays  ${{$job['salary']}} per year</p>
+</x-layout>
