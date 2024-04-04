20d77a7f8c2752d2f346bea96955f6497cad3c7e
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-04-04 10:54:32 +0200

    day  13 14 15 done

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,11 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->cursorPaginate(3);
+
     return view('jobs', [
-        'jobs' => Job::all()
-        ]);
+        'jobs' => $jobs
+    ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
