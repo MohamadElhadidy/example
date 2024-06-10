63c753180b5469898e806fffac9d1ee694d3c494
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-06-10 15:58:45 +0300

    register and logout done

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destroy()
+    {
+       Auth::logout();
+       return redirect('/');
+    }
+}
